5fb8004550d2734435d3c2b3134258ff670f35aa
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-11-23 08:26:50 +0200

    Add LottieAnimationManager and ImageManager for asset management

---
+++ lib/utils/constants/animations.dart
@@ -0,0 +1,5 @@
+class LottieAnimationManager {
+  const LottieAnimationManager._();
+  static const loading = 'assets/animations/loading.json';
+  static const splash = 'assets/animations/splash.json';
+}
---
+++ lib/utils/constants/images.dart
@@ -0,0 +1,7 @@
+class ImageManager {
+  const ImageManager._();
+  static const searchedLocation = 'assets/images/location.png';
+  static const splashMapLogo = 'assets/images/map1.png';
+  static const currentLocation = 'assets/images/pin1.png';
+
+}
