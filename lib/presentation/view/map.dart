4da4361bb321d49e1b762e107b6ccfea76f1969d
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-11-23 08:27:59 +0200

    Refactor MapScreen to integrate FloatingSearchBarWidget and enhance loading indicators

--- lib/presentation/view/map.dart
+++ lib/presentation/view/map.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-
 import 'package:provider/provider.dart';
-
 import '../view_model/map_view_model.dart';
-
+import '../widgets/floating_action_buttons.dart';
+import '../widgets/floating_search_bar.dart';
+import '../widgets/loding_indicator.dart';
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -15,19 +14,17 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late MapController _mapController;
-  late TextEditingController _searchController;
   bool _isInitialized = false;
+
   @override
   void initState() {
     super.initState();
-    _searchController = TextEditingController();
     _mapController = MapController();
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
     if (!_isInitialized) {
       Provider.of<MapViewModel>(context, listen: false).fetchCurrentLocation();
       _isInitialized = true;
@@ -36,17 +33,19 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<MapViewModel>(context, listen: false);
-    return Scaffold(
-      body: Consumer<MapViewModel>(
-        builder: (context, viewModel, child) {
-          if (viewModel.currentLocation == null) {
-            return const Center(child: CircularProgressIndicator());
-          }
+    final viewModel = Provider.of<MapViewModel>(context);
 
-          return Stack(
-            children: [
-              FlutterMap(
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Consumer<MapViewModel>(
+            builder: (context, viewModel, child) {
+              if (viewModel.currentLocation == null) {
+                return const Center(child: MyCircularLoadingIndicator());
+              }
+
+              return FlutterMap(
                 mapController: _mapController,
                 options: MapOptions(
                   initialCenter: viewModel.currentLocation!,
@@ -73,93 +72,25 @@ class _MapScreenState extends State<MapScreen> {
                     ],
                   ),
                 ],
-              ),
-              Positioned(
-                top: 50.0,
-                left: 10.0,
-                right: 10.0,
-                child: Card(
-                  child: ListTile(
-                    leading: const Icon(Icons.search),
-                    title: TextField(
-                      controller: _searchController,
-                      decoration: const InputDecoration(
-                        hintText: 'Search location',
-                        border: InputBorder.none,
-                      ),
-                      onChanged: (value) => viewModel.searchLocation(value),
-                    ),
-                    trailing: viewModel.isSearching
-                        ? const CircularProgressIndicator()
-                        : IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              _searchController.clear();
-                              viewModel.searchLocation('');
-                            },
-                          ),
-                  ),
-                ),
-              ),
-              if (viewModel.searchResults.isNotEmpty)
-                Positioned(
-                  top: 90.0,
-                  left: 10.0,
-                  right: 10.0,
-                  child: Card(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: viewModel.searchResults.length,
-                      itemBuilder: (context, index) {
-                        final result = viewModel.searchResults[index];
-                        return ListTile(
-                          title: Text(result.displayName),
-                          onTap: () {
-                            final point = LatLng(result.lat, result.lon);
-                            _mapController.move(point, 16.0);
-                            viewModel.getRoute(point);
-                            viewModel.searchLocation('');
-                            _searchController.clear();
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ),
-            ],
-          );
-        },
+              );
+            },
+          ),
+          FloatingSearchBarWidget(mapController: _mapController),
+        ],
       ),
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          FloatingActionButton.small(
-            heroTag: "search_location",
-            onPressed: () {
-              if (viewModel.destination != null) {
-                _mapController.move(viewModel.destination!, 16.0);
-              }
-            },
-            child: const Icon(Icons.search),
+          FloatingActionButton(
+            onPressed: () => viewModel.toggleDriverMode(),
+            tooltip: 'Toggle Driver Mode',
+            backgroundColor: viewModel.isDriverMode ? Colors.green : Colors.grey,
+            child: const Icon(Icons.drive_eta),
           ),
           const SizedBox(height: 10),
-          FloatingActionButton.small(
-            heroTag: "current_location",
-            onPressed: () {
-              if (viewModel.currentLocation != null) {
-                _mapController.move(viewModel.currentLocation!, 16.0);
-              }
-            },
-            child: const Icon(Icons.my_location),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton.small(
-            heroTag: "reset",
-            onPressed: () {
-              viewModel.resetMap();
-              _mapController.move(viewModel.currentLocation!, 10.0);
-            },
-            child: const Icon(Icons.refresh),
+          FloatingActionButtonWidgets(
+            viewModel: viewModel,
+            mapController: _mapController,
           ),
         ],
       ),
